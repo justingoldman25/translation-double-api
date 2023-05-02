ef4d711f17db075f336ad77bb28e3057e335049a
Justin K Goldman <jkg95784@uga.edu>
2023-05-02 12:21:25 -0400

    some visuals

--- src/main/java/cs1302/api/ApiApp.java
+++ src/main/java/cs1302/api/ApiApp.java
@@ -6,6 +6,9 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -16,6 +19,15 @@ public class ApiApp extends Application {
     Stage stage;
     Scene scene;
     VBox root;
+    HBox topBox;
+    HBox instructions;
+    HBox term;
+    HBox definition;
+    HBox spanish;
+    HBox french;
+    HBox bottomBar;
+    Button searchButton;
+    TextField searchBar;
 
 
     /**
@@ -24,11 +36,19 @@ public class ApiApp extends Application {
      */
     public ApiApp() {
         root = new VBox();
+        root.setPrefHeight(500);
+        topBox = new HBox();
+        instructions = new HBox();
+        term = new HBox();
+        definition = new HBox();
+        spanish = new HBox();
+        french = new HBox();
+        bottomBar = new HBox();
+        searchButton = new Button();
+        searchBar = new TextField();
+
     } // ApiApp
 
-    private void searchDictionary() {
-        return;
-    }
 
     /** {@inheritDoc} */
     @Override
@@ -46,9 +66,19 @@ public class ApiApp extends Application {
         Label notice = new Label("Modify the starter code to suit your needs.");
 
         // setup scene
-        root.getChildren().addAll(banner, notice);
+        root.getChildren().addAll(topBox, instructions, term, definition, spanish, french);
+        root.getChildren().add(bottomBar);
         scene = new Scene(root);
 
+        //setup top bar
+        Label termLabel = new Label("Term:");
+        Button searchButton = new Button("Search");
+        topBox.setSpacing(50);
+        topBox.getChildren().addAll(termLabel, searchBar, searchButton);
+
+        //setup instructions bar
+        Label instructionsLabel = new Label("Type your term into the box and press search.");
+        instructions.getChildren().add(instructionsLabel);
         // setup stage
         stage.setTitle("ApiApp!");
         stage.setScene(scene);
